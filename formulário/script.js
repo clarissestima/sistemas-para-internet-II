108b2206b7522c7ed8aa85752f778c699fd4e6e2
Clarisse Estima <108906349+clarissestima@users.noreply.github.com>
2024-03-20 16:25:14 -0300

    Add files via upload

---
+++ formulário/script.js
@@ -0,0 +1,27 @@
+const cpfInput = document.getElementById('cpf')
+cpfInput.addEventListener('input', function(event) {
+    let valor = event.target.value;
+    valor = valor.replace(/\D/g, '');
+    valor = valor.slice(0, 11);
+
+    //###.###.###-##
+    valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+    valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+    valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+    event.target.value = valor;
+})
+
+
+const telefoneInput = document.getElementById('tel');
+telefoneInput.addEventListener('input', function(event) {
+    let valor = event.target.value;
+    valor = valor.replace(/\D/g, '');
+    valor = valor.slice(0, 11);
+
+    //(##) #####-####
+    valor = valor.replace(/^(\d{2})(\d)/g, '($1) $2');
+    valor = valor.replace(/(\d{5})(\d)/, '$1-$2');
+
+    event.target.value = valor;
+});
